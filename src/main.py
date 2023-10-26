212e684273dee3355bb8084283843181e0901aec
zSnails <erizojuan33@gmail.com>
2023-10-26 01:25:49 -0600

    fix: extract db and prolog initial instantiations so that they can be used in interactive mode

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from app import Program
+from app.sqlite import SQLite
+from app.prolog import Prolog
 
+
+db = SQLite("./testdb.sqlite")
+prolog = Prolog()
 
 if __name__ == "__main__":
-    Program().run()
+    try:
+        Program().run()
+    except KeyboardInterrupt:
+        prolog.close()
+        db.close()
