1e100284235f329891f198abcee9718a1eb2442e
zSnails <erizojuan33@gmail.com>
2023-10-29 20:37:08 -0600

    fix(thread): fix ProgrammingError thrown by the sqlite3 module

--- src/app/sqlite.py
+++ src/app/sqlite.py
@@ -13,22 +13,15 @@ class SQLite(metaclass=SingletonMeta):
             return
         self.db_name = db_name
         self.connection = sqlite3.connect(db_name, check_same_thread=False)
-        self.connection.execute("PRAGMA foreign_keys = on;")
-        self.connection.execute("PRAGMA encoding = 'UTF-8';")
-        self.cursor = self.connection.cursor()
+        self.connection.execute("PRAGMA foreign_keys=ON;")
+        self.connection.execute("PRAGMA encoding='UTF-8';")
 
     def execute(self: Self, query: str, values: List = []) -> Cursor:
         """Execute a query."""
-        cur = self.cursor.execute(query, values)
-        return cur
-
-    def fetch_all(self: Self) -> List:
-        """Fetch all results."""
-        return self.cursor.fetchall()
+        return self.connection.execute(query, values)
 
     def close(self: Self) -> None:
         """Close the SQLite connection."""
-        self.cursor.close()
         self.connection.close()
 
         del SingletonMeta._instances[self.__class__]
@@ -37,10 +30,6 @@ class SQLite(metaclass=SingletonMeta):
 if __name__ == "__main__":
     # Initialize
     sqlite = SQLite("../testdb.sqlite")
-
-    # Execute
-    sqlite.execute("SELECT * FROM Foods")
-    print(sqlite.fetch_all())
 
     # Close
     sqlite.close()
